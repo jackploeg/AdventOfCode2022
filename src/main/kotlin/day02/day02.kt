ef4bbede7bd61c972a6cf666b69b5d534bdca440
Jack Ploeg <jack.ploeg@gmail.com>
2022-12-03 17:46:00 +0100

    Day 02 en 03

---
+++ src/main/kotlin/day02/day02.kt
@@ -0,0 +1,110 @@
+package day02
+
+import utitlities.readStringFile
+
+fun main() {
+    println(getScore1("src/main/kotlin/day02/test.txt"))
+    println(getScore1("src/main/kotlin/day02/input.txt"))
+
+    println(getScore2("src/main/kotlin/day02/test.txt"))
+    println(getScore2("src/main/kotlin/day02/input.txt"))
+
+}
+
+fun getScore1(fileName: String): Int {
+    val moves = readStrategy(fileName)
+    return moves.map { calculateScore1(it) }
+        .reduce { sum, score -> sum + score}
+}
+
+fun getScore2(fileName: String): Int {
+    val moves = readStrategy(fileName)
+    return moves.map { calculateScore2(it) }
+        .reduce { sum, score -> sum + score}
+}
+
+fun readStrategy(fileName: String): List<Pair<Char, Char>> {
+    val moves: MutableList<Pair<Char, Char>> = mutableListOf()
+    val pairs = readStringFile(fileName)
+    for (pair in pairs) {
+        moves.add(Pair(pair[0], pair[2]))
+    }
+    return moves
+}
+
+fun calculateScore1(move: Pair<Char, Char>): Int {
+    val (a, b) = move
+    var score = when (a) {
+        'A' ->
+            when (b) {
+                'X' -> 3
+                'Y' -> 6
+                'Z' -> 0
+                else -> -1
+            }
+        'B' ->
+            when (b) {
+                'X' -> 0
+                'Y' -> 3
+                'Z' -> 6
+                else -> -1
+            }
+        'C' ->
+            when (b) {
+                'X' -> 6
+                'Y' -> 0
+                'Z' -> 3
+                else -> -1
+            }
+        else -> -1
+    }
+
+    score += when (b) {
+        'X' -> 1
+        'Y' -> 2
+        'Z' -> 3
+        else -> -1
+    }
+
+    return score
+}
+
+fun calculateScore2(move: Pair<Char, Char>): Int {
+    val (a, b) = move
+    // a = rock, b = paper, c = scissors
+    // x = lose, y = draw, z = win
+    // 1 = rock, 2 = paper, 3 = scissors
+    val myMove = when (a) {
+        'A' ->
+            when (b) {
+                'X' -> 3
+                'Y' -> 1
+                'Z' -> 2
+                else -> -1
+            }
+        'B' ->
+            when (b) {
+                'X' -> 1
+                'Y' -> 2
+                'Z' -> 3
+                else -> -1
+            }
+        'C' ->
+            when (b) {
+                'X' -> 2
+                'Y' -> 3
+                'Z' -> 1
+                else -> -1
+            }
+        else -> -1
+    }
+
+    val score = myMove + when (b) {
+        'X' -> 0
+        'Y' -> 3
+        'Z' -> 6
+        else -> -1
+    }
+
+    return score
+}
---
+++ src/main/kotlin/day03/day03.kt
@@ -0,0 +1,52 @@
+package day03
+
+import utitlities.readStringFile
+
+fun main() {
+    println(getSumOfPriorities("src/main/kotlin/day03/test.txt"))
+    println(getSumOfPriorities("src/main/kotlin/day03/input.txt"))
+
+    println(getSumOfBadgePriorities("src/main/kotlin/day03/test.txt"))
+    println(getSumOfBadgePriorities("src/main/kotlin/day03/input.txt"))
+
+}
+
+fun getSumOfPriorities(fileName: String): Int {
+    val rucksacks = readStringFile(fileName)
+    return rucksacks.map { determineRucksackPriority(it) }
+        .reduce { sum, priority -> sum + priority}
+}
+
+fun getSumOfBadgePriorities(fileName: String): Int {
+    val rucksacks = readStringFile(fileName)
+    val elveGroups = rucksacks.chunked(3)
+    return elveGroups.map { determineElveGroupPriority(it) }
+        .reduce { sum, priority -> sum + priority}
+}
+fun determineRucksackPriority(rucksack: String): Int {
+    val contentLength = rucksack.length / 2
+    val compartment1Content = rucksack.substring(0, contentLength)
+    val compartment2Content = rucksack.substring(contentLength, rucksack.length)
+    val commonItems = compartment1Content.toCharArray()
+        .filter { compartment2Content.toCharArray().contains(it) }
+        .distinct()
+
+    return getContentPriority(commonItems[0])
+}
+
+fun determineElveGroupPriority(group: List<String>): Int {
+    val commonItems = group.get(0).toCharArray()
+        .filter { group.get(1).toCharArray().contains(it) }
+        .filter { group.get(2).toCharArray().contains(it) }
+        .distinct()
+
+    return getContentPriority(commonItems[0])
+}
+
+fun getContentPriority(content: Char): Int {
+    if (content in 'a'..'z') {
+        return content.code - 96
+    } else {
+        return content.code - 64 + 26
+    }
+}
