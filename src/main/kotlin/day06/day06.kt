6c79bb97940a929d63a3551e2d145faac2645e5d
Jack Ploeg <jack.ploeg@gmail.com>
2022-12-06 08:54:27 +0100

    Day 06

---
+++ src/main/kotlin/day06/day06.kt
@@ -0,0 +1,34 @@
+package day06
+
+import utitlities.readStringFile
+
+fun main() {
+    findmarker(4, "src/main/kotlin/day06/test.txt")
+    findmarker(4, "src/main/kotlin/day06/input.txt")
+    findmarker(14, "src/main/kotlin/day06/test.txt")
+    findmarker(14, "src/main/kotlin/day06/input.txt")
+}
+
+fun findmarker(markersize: Int, fileName: String) {
+    val input = readStringFile(fileName)
+    for(line in input) {
+        val marker: ArrayDeque<Char> = ArrayDeque(markersize)
+        val lineChars = line.toCharArray()
+        for (i in 1..markersize) {
+            marker.add(lineChars.get(i-1))
+        }
+        var markerEnd = markersize
+        while (!allCharsDiffer(marker)) {
+            val c = lineChars.get(markerEnd)
+            marker.removeFirst()
+            marker.add(c)
+            markerEnd++
+        }
+        println(marker)
+        println(markerEnd)
+    }
+}
+
+fun allCharsDiffer(chars: ArrayDeque<Char>): Boolean =
+    chars.distinct().size == chars.size
+
---
+++ src/main/kotlin/template/template.kt
@@ -0,0 +1,12 @@
+package day
+
+import utitlities.readStringFile
+
+fun main() {
+    println(xxx("src/main/kotlin/day/test.txt"))
+}
+
+fun xxx(fileName: String) {
+    val input = readStringFile(fileName)
+
+}
